ff0f03fd6fe7ab660e3d02fc8f7c24d6300c6bce
es47 <kl8534ghj4633@gmail.com>
2019-06-02 22:16:05 +0800

    add

---
+++ 小畫家/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace 小畫家
+{
+    partial class From1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.檔案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開新檔案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開啟舊檔ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.另存新檔ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.結束ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.筆畫設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.顏色ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.筆寬ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.檔案ToolStripMenuItem,
+            this.筆畫設定ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 29);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 檔案ToolStripMenuItem
+            // 
+            this.檔案ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開新檔案ToolStripMenuItem,
+            this.開啟舊檔ToolStripMenuItem,
+            this.另存新檔ToolStripMenuItem,
+            this.結束ToolStripMenuItem});
+            this.檔案ToolStripMenuItem.Name = "檔案ToolStripMenuItem";
+            this.檔案ToolStripMenuItem.Size = new System.Drawing.Size(54, 25);
+            this.檔案ToolStripMenuItem.Text = "檔案";
+            // 
+            // 開新檔案ToolStripMenuItem
+            // 
+            this.開新檔案ToolStripMenuItem.Name = "開新檔案ToolStripMenuItem";
+            this.開新檔案ToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.開新檔案ToolStripMenuItem.Text = "開新檔案";
+            this.開新檔案ToolStripMenuItem.Click += new System.EventHandler(this.開新檔案ToolStripMenuItem_Click);
+            // 
+            // 開啟舊檔ToolStripMenuItem
+            // 
+            this.開啟舊檔ToolStripMenuItem.Name = "開啟舊檔ToolStripMenuItem";
+            this.開啟舊檔ToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.開啟舊檔ToolStripMenuItem.Text = "開啟舊檔";
+            this.開啟舊檔ToolStripMenuItem.Click += new System.EventHandler(this.開啟舊檔ToolStripMenuItem_Click);
+            // 
+            // 另存新檔ToolStripMenuItem
+            // 
+            this.另存新檔ToolStripMenuItem.Name = "另存新檔ToolStripMenuItem";
+            this.另存新檔ToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.另存新檔ToolStripMenuItem.Text = "另存新檔";
+            this.另存新檔ToolStripMenuItem.Click += new System.EventHandler(this.另存新檔ToolStripMenuItem_Click);
+            // 
+            // 結束ToolStripMenuItem
+            // 
+            this.結束ToolStripMenuItem.Name = "結束ToolStripMenuItem";
+            this.結束ToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.結束ToolStripMenuItem.Text = "結束";
+            this.結束ToolStripMenuItem.Click += new System.EventHandler(this.結束ToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "|*.jpg";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "|*.jpg";
+            // 
+            // 筆畫設定ToolStripMenuItem
+            // 
+            this.筆畫設定ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.顏色ToolStripMenuItem,
+            this.筆寬ToolStripMenuItem});
+            this.筆畫設定ToolStripMenuItem.Name = "筆畫設定ToolStripMenuItem";
+            this.筆畫設定ToolStripMenuItem.Size = new System.Drawing.Size(86, 25);
+            this.筆畫設定ToolStripMenuItem.Text = "筆畫設定";
+            // 
+            // 顏色ToolStripMenuItem
+            // 
+            this.顏色ToolStripMenuItem.Name = "顏色ToolStripMenuItem";
+            this.顏色ToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.顏色ToolStripMenuItem.Text = "顏色";
+            this.顏色ToolStripMenuItem.Click += new System.EventHandler(this.顏色ToolStripMenuItem_Click);
+            // 
+            // 筆寬ToolStripMenuItem
+            // 
+            this.筆寬ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
+            this.筆寬ToolStripMenuItem.Name = "筆寬ToolStripMenuItem";
+            this.筆寬ToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.筆寬ToolStripMenuItem.Text = "筆寬";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "5",
+            "10",
+            "20"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 24);
+            this.toolStripComboBox1.Text = "1";
+            // 
+            // From1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "From1";
+            this.Text = "小畫家";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.From1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 檔案ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開新檔案ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開啟舊檔ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 另存新檔ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 結束ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem 筆畫設定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 顏色ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 筆寬ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ 小畫家/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 小畫家
+{
+    public partial class From1 : Form
+    {
+        public From1()
+        {
+            InitializeComponent();
+        }
+
+        private void 開新檔案ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(800, 600);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+        }
+
+        private void From1_Load(object sender, EventArgs e)
+        {
+            開新檔案ToolStripMenuItem_Click(sender, e);
+        }
+
+        private void 開啟舊檔ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void 另存新檔ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                bmp.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        int x0, y0;
+        Bitmap bg;
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Graphics g = Graphics.FromImage(pictureBox1.Image);
+                Pen p = new Pen(colorDialog1.Color, int.Parse(toolStripComboBox1.Text));
+                g.DrawLine(p,x0, y0, e.X, e.Y);
+                x0 = e.X;
+                y0 = e.Y;
+                pictureBox1.Refresh();
+            }
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                Bitmap bf = new Bitmap(bg);
+                Graphics g2 = Graphics.FromImage(bf);
+                Brush bush = new SolidBrush(colorDialog1.Color);
+                g2.FillEllipse(bush, x0, y0, Math.Abs(e.X - x0), Math.Abs(e.Y - y0));
+                pictureBox1.Image = bf;
+
+            }
+        }
+
+        private void 顏色ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+        }
+
+        private void 結束ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x0 = e.X;
+            y0 = e.Y;
+            bg = new Bitmap(pictureBox1.Image);
+        }
+    }
+}
